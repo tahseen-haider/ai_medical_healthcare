26492fe51d44a6d1c8730f36f2bacdb98be0a4d9
Tahsin Haider <tahsin3194@gmail.com>
2025-05-16 00:02:49 +0500

    use prisma for database query using accelerator

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-
 
 export default async function Home() {
-  const prisma = new PrismaClient().$extends(withAccelerate())
-
-  const newPatient = await prisma.patient.create({
-    data: {
-      name: "John Doe",
-      email: "john@doe.com",
-      birthDate: new Date("2002-11-30")
-    }
-  });
-
-  console.log("Patient Created: ", newPatient)
   return (
     <div>
       HOME PAGE
