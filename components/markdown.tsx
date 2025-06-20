237b5b8ccc68f5c0a45d97c98d3ebc2f2f2c2bf4
Tahsin Haider <tahsin3194@gmail.com>
2025-06-20 16:02:13 +0500

    fix: while marking down assistant message, the message box was empty

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -6,17 +6,20 @@ import { useEffect, useState } from "react";
 
 export default function Markdown({ text }: { text: string }) {
   const [secureHTML, setSecureHTML] = useState("");
+  const [isMarkedDown, setIsMarkedDown] = useState(false);
 
   useEffect(() => {
     async function parseAndSanitize() {
       const rawHTML = await marked.parse(text); // Await the async markdown parsing
       const cleanHTML = DOMPurify.sanitize(rawHTML); // Sanitize the HTML string
+      setIsMarkedDown(true)
       setSecureHTML(cleanHTML); // Store in state
     }
 
     parseAndSanitize();
   }, [text]);
 
+  if(!isMarkedDown) return <div>Loading...</div>
   return (
     <div
       dangerouslySetInnerHTML={{ __html: secureHTML }}
